d49298592ab261b3911c5112ca0f878ea714cb3a
Martin Kolenič <kolenic.martin@gmail.com>
2023-01-11 16:06:30 +0100

    Add Day 1 project

---
+++ d1-bandname.py
@@ -0,0 +1,12 @@
+# 1OO Days of Code: Band Name Generator project
+#
+# This simply generates a band name suggestion by
+# combining your hometown and pet names as per
+# the input.
+
+print("Welcome to the Band Name Generator")
+
+town = input("What's the name of the city up in?\n")
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + town + " " + pet)
